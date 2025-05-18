f5234213f85d56f332e4b37f4791149e8b8687a7
cmsilen <carlomazzanti00@gmail.com>
2025-05-18 13:06:52 +0200

    grafici v1 ed espansione dei test

--- test.bat
+++ test.bat
@@ -16,7 +16,7 @@ REM Compila con gcc
 call compile %~1
 
 
-for %%i in (4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048) do (
+for %%i in (4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384) do (
     for %%j in (3,15,30) do (
         echo Esecuzione: main %%i blocks %%j imgs
         main %%i %%j 1
